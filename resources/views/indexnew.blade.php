4738a02d476e1554291acb05d67d69adf6cc600a
bringforthjoy <adelugba.emma@gmail.com>
2019-11-03 18:51:57 +0100

    new landing page

--- resources/views/indexnew.blade.php
+++ resources/views/indexnew.blade.php
@@ -101,14 +101,14 @@
             @if (Route::has('login'))
                 <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
                     <li class="nav-item active">
-                        <a class="nav-link text-primary pr-5" href="#">Posts <span class="sr-only">(current)</span></a>
+                        <a class="nav-link text-primary pr-5" href="{{ url('/post') }}">Posts <span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link pr-5 text-primary" href="#">Login</a>
+                        <a class="nav-link pr-5 text-primary" href="{{ route('login') }}">Login</a>
                     </li>
                     @if (Route::has('register'))
                     <li class="nav-item">
-                        <a class="nav-link text-primary  pr-2" href="#">Register</a>
+                        <a class="nav-link text-primary  pr-2" href="{{ route('register') }}">Register</a>
                     </li>
                     @endif
                     @endauth
